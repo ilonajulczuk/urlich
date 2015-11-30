3b959311a99b328f9fb5f3b086ff678fd2e3b68b
Justyna Ilczuk <att@atte.ro>
2015-11-30 22:01:43 +0100

    Add json value of the created page.

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -61,6 +61,7 @@ func (c *PageController) AddHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json; charset=UTF=8")
 	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(page)
 	log.Println("Created a page", page)
 }
 
